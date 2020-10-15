ae675a6f20fcd9624cc8ba133c4945188781b84d
Harvey Moon <harveymoons@gmail.com>
2020-10-16 00:02:41 +0900

    test grid.js

--- public/js/memberList.js
+++ public/js/memberList.js
@@ -16,7 +16,8 @@ function getMemberList() {
         })
         .then(data => {
             if (data) {
-                loadMemberList(data);
+                testGrirdJS(data);
+                // loadMemberList(data);
             }
         })
         .catch(error => {
@@ -25,17 +26,91 @@ function getMemberList() {
 }
 
 function loadMemberList(data) {
-    const tableContainer = document.getElementById("handsonTable");
+    const tableContainer = document.getElementById("dataTable");
 
     new Handsontable(tableContainer, {
         data: data,
         bindRowsWithHeaders: true,
         rowHeaders: true,
-        colHeaders: ["Image", "Name", "Contact", "Address", "Gender", "Birthday", "Marital Status", "Faith State"],
-        preventOverflow: "vertical",
-        height: "100vh",
-        autoColumnSize: { useHeaders: true },
+        height: window.innerHeight,
+        stretchH: "all",
         readOnly: true,
         licenseKey: "non-commercial-and-evaluation",
+        maxRows: 20,
+        autoColumnSize: true,
+        colHeaders: ["Image", "Name", "Contact", "Address", "Gender", "Birthday", "Marital Status", "Faith State"],
+        columns: [
+            { data: null, renderer: imageRenderer },
+            { data: "name" },
+            { data: "contact", renderer: contactRenderer, autoColumnSize: true },
+            { data: "address1" },
+            { data: "gender" },
+            { data: "birthday" },
+            { data: "married" },
+            { data: "martialStatus" },
+            { data: "faithState" },
+        ],
+        manualRowResize: true,
+        manualColumnResize: true,
     });
+}
+
+// hansdonTable Renderer
+function imageRenderer(instance, td, row, col, prop, value, cellProperties) {
+    if (value) {
+        const textNode = document.createTextNode(value);
+        td.appendChild(textNode);
+    } else {
+        const textNode = document.createTextNode(null);
+        td.appendChild(textNode);
+    }
+}
+
+function contactRenderer(instance, td, row, col, prop, value, cellProperties) {
+    if (value) {
+        const contact = [value.contact1, value.contact2, value.contact3].join("-");
+        const textNode = document.createTextNode(contact);
+        td.appendChild(textNode);
+    }
+}
+
+// grid.js
+function testGrirdJS(data) {
+    new gridjs.Grid({
+        data: data,
+        columns: [
+            { name: "Image", id: "imagePath" },
+            { name: "Name", id: "name" },
+            { name: "Contact", id: "contact", formatter: contactFormatter },
+            { name: "Address", id: "address1" },
+            { name: "Gender", id: "gender" },
+            { name: "Birthday", id: "birthday" },
+            { name: "Marital Status", id: "married" },
+            { name: "Faith State", id: "faithState" },
+        ],
+        data: data,
+        pagination: true,
+        fixedHeader: true,
+        sort: false,
+        search: {
+            enabled: true,
+        },
+        language: {
+            search: {
+                placeholder: "🔍 Search...",
+            },
+            pagination: {
+                previous: "<<",
+                next: ">>",
+                showing: "😃 Displaying",
+                results: () => "Records",
+            },
+        },
+    }).render(document.getElementById("dataTable"));
+}
+
+function contactFormatter(data) {
+    if (data) {
+        return [data.contact1, data.contact2, data.contact3].join("-");
+    }
 }
